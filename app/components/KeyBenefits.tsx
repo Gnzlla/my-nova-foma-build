09f34a5e2e4153f2fb24d403daab6e2294a9e9ac
Ellaine Gonzales <ellainegonzales003@gmail.com>
2025-12-19 09:09:29 +0800

    update hover and animation

--- app/components/KeyBenefits.tsx
+++ app/components/KeyBenefits.tsx
@@ -14,7 +14,7 @@ export default function KeyBenefitsSection() {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".benefits-container",
-        start: "top top",
+        start: "top 40%",
       },
     });
 
@@ -34,47 +34,52 @@ export default function KeyBenefitsSection() {
 
   return (
     <>
-      <section className="benefits-container h-screen w-full">
+      <section className="benefits-container py-19 md:py-20">
         <div className="inside-benefits w-9/10">
           <h2 className="leading-none text-4xl font-bold">
             KEY BENEFITS OF <br /> NOVA FOMA
           </h2>
-          <div className="benefit grid grid-cols-2 gap-5 text-white lg:grid-cols-4">
-            <div
-              style={{
-                opacity: isMouseOver ? 1.0 : 0.5,
-                transition: "opacity 0.2s ease-out",
-              }}
-              className="circular-img lg:gap-6"
-              onMouseOver={handleMouseOver}
-              onMouseOut={handleMouseOut}
-            >
-              <img src="../skin-rejuventaion.jpg" alt="skin-rejuvenation-img" />
-              <div>
+          <div className="benefit grid grid-cols-2 gap-5 text-white lg:grid-cols-4 ">
+            <div className="circular-img group lg:gap-6">
+              <img
+                src="../skin-rejuventaion.jpg"
+                alt="skin-rejuvenation-img"
+                className="opacity-50 group-hover:opacity-100 transition-opacity duration-150"
+              />
+              <div className="opacity-50 group-hover:opacity-100">
                 <h3>Skin Rejuvenation</h3>
                 <h4>피부 회복</h4>
               </div>
             </div>
-            <div className="circular-img lg:gap-6 ">
-              <img src="../elasticity.jpg" alt="elasticity image" />
-              <div>
+            <div className="circular-img  group lg:gap-6 ">
+              <img
+                src="../elasticity.jpg"
+                alt="elasticity image"
+                className="opacity-50 group-hover:opacity-100 transition-opacity duration-150"
+              />
+              <div className="opacity-50 group-hover:opacity-100 transition-opacity duration-150">
                 <h3>Elasticity</h3>
                 <h4>홍조 및 염증 완화</h4>
               </div>
             </div>
-            <div className="circular-img lg:gap-6">
-              <img src="../collagen.jpg" alt="collagen image" />
-              <div>
+            <div className="circular-img group lg:gap-6">
+              <img
+                src="../collagen.jpg"
+                alt="collagen image"
+                className="opacity-50 group-hover:opacity-100 transition-opacity duration-150"
+              />
+              <div className="opacity-50 group-hover:opacity-100 transition-opacity duration-150">
                 <h3>Collagen Boost</h3>
                 <h4>콜라겐 촉진</h4>
               </div>
             </div>
-            <div className="circular-img lg:gap-6">
+            <div className="circular-img  group lg:gap-6">
               <img
                 src="../cellular-vitality.jpg"
                 alt="cellular vitality image"
+                className="opacity-50 group-hover:opacity-100 transition-opacity duration-150"
               />
-              <div>
+              <div className="opacity-50 group-hover:opacity-100 transition-opacity duration-150">
                 <h3>Cellular Vitality</h3>
                 <h4>세포 활력 충전</h4>
               </div>
