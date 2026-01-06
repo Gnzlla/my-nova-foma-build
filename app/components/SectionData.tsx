92aa29f7e91e8d1c20bc3bc9e7e7db37670b2eb3
Ellaine Gonzales <ellainegonzales003@gmail.com>
2026-01-06 17:16:56 +0800

    Summary of data props

---
+++ app/components/SectionData.tsx
@@ -0,0 +1,105 @@
+import { link } from "fs";
+
+export const downloadRowContent = [
+  {
+    header: "NOVA FOMA PPT",
+    subheader: "노바포마 소개서 다운로드",
+    link: "https://media.tenor.com/bkl4kbX9g8EAAAAM/speedddd.gif",
+  },
+
+  {
+    header: "NOVA FOMA REFLEAT",
+    subheader: "노바포마 소개서 다운로드",
+    link: "https://media.tenor.com/z1MHq4-Yep8AAAAe/shrek-smirk.png",
+  },
+
+  {
+    header: "NOVA FOMA MANUAL",
+    subheader: "노바포마 사용설명서 다운로드",
+    link: "https://pbs.twimg.com/media/FC1RfxgWUAMr_qY.jpg",
+  },
+];
+
+export const tableRows = [
+  {
+    label: "유효기간",
+    values: [
+      { icon: "/cross.svg", text: "약 2~4주" },
+      { icon: "/check.svg", text: "약 6~7개월" },
+    ],
+  },
+  {
+    label: "피부 투과성",
+    values: [
+      { icon: "/cross.svg", text: "표피층 한정" },
+      { icon: "/check.svg", text: "진피층 도달" },
+    ],
+  },
+  {
+    label: "활성 지속 시간",
+    values: [
+      { icon: "/cross.svg", text: "1~2시간" },
+      { icon: "/check.svg", text: "18시간 이상 지속 방출" },
+    ],
+  },
+  {
+    label: "항염·항산화 효과",
+    values: [
+      { icon: "/cross.svg", text: "미약" },
+      { icon: "/check.svg", text: "NAD⁺ 활성 증가" },
+    ],
+  },
+  {
+    label: "안정성 등급",
+    values: [
+      { icon: "/cross.svg", text: "낮음" },
+      { icon: "/check.svg", text: "매우 높음" },
+    ],
+  },
+];
+
+export const KeyRows = [
+  {
+    imgSrc: "../skin-rejuventaion.jpg",
+    title: "Skin Rejuvenation",
+    subheader: "피부 회복",
+  },
+  {
+    imgSrc: "../elasticity.jpg",
+    title: "Elasticity",
+    subheader: "홍조 및 염증 완화",
+  },
+  {
+    imgSrc: "../collagen.jpg",
+    title: "Collagen Boost",
+    subheader: "콜라겐 촉진",
+  },
+  {
+    imgSrc: "../cellular-vitality.jpg",
+    title: "Cellular Vitality",
+    subheader: "세포 활력 충전",
+  },
+];
+
+export const footerRows = [
+  {
+    link: "https://pf.kakao.com/_TsIAE",
+    imgSrc: "../footer-assets/footer-kakao.png",
+    alt: "Kakao Talk",
+  },
+  {
+    link: "https://www.instagram.com/",
+    imgSrc: "../footer-assets/footer-instagram.png",
+    alt: "Instagram",
+  },
+  {
+    link: "https://www.youtube.com/",
+    imgSrc: "../footer-assets/footer-youtube.png",
+    alt: "Youtube",
+  },
+  {
+    link: "https://section.blog.naver.com/BlogHome.naver?directoryNo=0&currentPage=1&groupId=0",
+    imgSrc: "../footer-assets/footer-blog.png",
+    alt: "Naver Blog",
+  },
+];
